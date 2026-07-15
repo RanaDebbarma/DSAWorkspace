fd564f1e378ab3634db9abd99edf46e0d9162f3d
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-07-16 05:15:37 +0530

    merge-ll update

--- NeetCode150/6.Linked List/2.merge-two-sorted-lists.ts
+++ NeetCode150/6.Linked List/2.merge-two-sorted-lists.ts
@@ -1,44 +1,56 @@
-import { runTests, TestCase } from "#functions/code-tester.js";
-import { createLinkedList, ListNode, printLinkedList } from "#functions/linked-list.js";
+import { runTests } from "#functions/code-tester.js";
+import { createLinkedList, ListNode } from "#functions/linked-list.js";
 
 // LeetCode 21
 
-const myLink = function mergeTwoLists(
+function mergeTwoLists(
   list1: ListNode | null,
   list2: ListNode | null,
 ): ListNode | null {
+  // ---------------------- Approach 1 ------------------
+  if (!list1) return list2;
+  if (!list2) return list1;
+  
   const dummy = new ListNode(0);
-  let tail = dummy;
+  let curr = dummy;
 
   while (list1 && list2) {
     if (list1.val <= list2.val) {
-      tail.next = list1;
+      curr.next = list1;
       list1 = list1.next;
     } else {
-      tail.next = list2;
+      curr.next = list2;
       list2 = list2.next;
     }
-    tail = tail.next;
+    curr = curr.next;
   }
 
-  tail.next = list1 ?? list2;
-  
+  curr.next = list1 ?? list2;
+
   return dummy.next;
+
+  // ---------------------- Approach 1 ------------------
 }
 
-const ll_inputs: TestCase<typeof myLink>[] = [
-  {
-    input: [createLinkedList([1, 2, 4]), createLinkedList([1, 3, 4])],
-    output: createLinkedList([1, 1, 2, 3, 4, 4]),
-  },
-  {
-    input: [createLinkedList([]), createLinkedList([])],
-    output: createLinkedList([]),
-  },
-  {
-    input: [createLinkedList([]), createLinkedList([0])],
-    output: createLinkedList([0]),
-  },
-];
-
-runTests(myLink, ll_inputs, true);
+runTests(
+  mergeTwoLists,
+  [
+    {
+      input: [createLinkedList([1, 2, 4]), createLinkedList([1, 3, 4])],
+      output: createLinkedList([1, 1, 2, 3, 4, 4]),
+    },
+    {
+      input: [createLinkedList([]), createLinkedList([])],
+      output: createLinkedList([]),
+    },
+    {
+      input: [createLinkedList([]), createLinkedList([0])],
+      output: createLinkedList([0]),
+    },
+    {
+      input: [createLinkedList([0]), createLinkedList([0])],
+      output: createLinkedList([0, 0]),
+    },
+  ],
+  true,
+);
