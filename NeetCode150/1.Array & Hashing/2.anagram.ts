d4359db35a7a41c13481d35b56a099c434c22b4e
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-08-09 21:21:16 +0530

    anagram

--- NeetCode150/1.Array & Hashing/2.anagram.ts
+++ NeetCode150/1.Array & Hashing/2.anagram.ts
@@ -1,39 +1,41 @@
 import { runTests } from "#functions/code-tester.js";
 
-// function isAnagram(s: string, t: string): boolean {
-//   if (s.length !== t.length) return false;
-
-//   const count = new Array<number>(26).fill(0);
-//   for (let i = 0; i < s.length; i++) {
-//     const idx = s.codePointAt(i)! - "a".codePointAt(0)!;
-//     count[idx]++;
-//   }
-//   for (let i = 0; i < t.length; i++) {
-//     const idx = t.codePointAt(i)! - "a".codePointAt(0)!;
-//     count[idx]--;
-//     if (count[idx] === -1) return false;
-//   }
-
-//   return true;
-// }
-
-// for unicode
 function isAnagram(s: string, t: string): boolean {
   if (s.length !== t.length) return false;
 
-  const count = new Map<string, number>();
+  const count = new Int32Array(26);
+  const OFFSET = 97
 
-  for (const ch of s) {
-    count.set(ch, (count.get(ch) ?? 0) + 1);
+  for (let i = 0; i < s.length; i++) {
+    const idx = s.codePointAt(i)! - OFFSET;
+    count[idx]++;
+  }
+  for (let i = 0; i < t.length; i++) {
+    const idx = t.codePointAt(i)! - OFFSET;
+    if (count[idx] === 0) return false;
+    count[idx]--;
   }
 
-  for (const ch of t) {
-    const freq = count.get(ch)!;
-    if (!freq) return false;
-    count.set(ch, freq - 1);
-  }
   return true;
 }
+
+// for unicode
+// function isAnagram(s: string, t: string): boolean {
+//   if (s.length !== t.length) return false;
+
+//   const count = new Map<string, number>();
+
+//   for (const ch of s) {
+//     count.set(ch, (count.get(ch) ?? 0) + 1);
+//   }
+
+//   for (const ch of t) {
+//     const freq = count.get(ch)!;
+//     if (!freq) return false;
+//     count.set(ch, freq - 1);
+//   }
+//   return true;
+// }
 
 runTests(isAnagram, [
   { input: ["anagram", "nagaram"], output: true },
