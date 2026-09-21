bb3032a3b13e30b8b308fb153f56a280fddfba6c
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-09-21 15:32:08 +0530

    leetcode 703 soln

--- NeetCode150/10.Heap-Priority-Queue/1.kth-largest-element-in-a-stream.ts
+++ NeetCode150/10.Heap-Priority-Queue/1.kth-largest-element-in-a-stream.ts
@@ -3,11 +3,77 @@ import { runClassTests } from "#functions/code-tester.js";
 // LeetCode 703
 
 class KthLargest {
-  // Implement class here...
-  constructor(k: number, nums: number[]) {}
+  private heap: number[] = [];
+  private readonly k: number;
+
+  constructor(k: number, nums: number[]) {
+    this.k = k;
+
+    for (const num of nums) {
+      this.insert(num);
+
+      if (this.heap.length > k) {
+        this.pop();
+      }
+    }
+  }
 
   add(val: number): number {
-    return 0;
+    this.insert(val);
+
+    if (this.heap.length > this.k) {
+      this.pop();
+    }
+
+    return this.heap[0];
+  }
+
+  private insert(val: number): void {
+    this.heap.push(val);
+    this.bubbleUp(this.heap.length - 1);
+  }
+
+  private swap(first: number, second: number): void {
+    [this.heap[first], this.heap[second]] = [
+      this.heap[second],
+      this.heap[first],
+    ];
+  }
+
+  private bubbleUp(i: number): void {
+    if (i === 0) return;
+
+    const p = (i - 1) >> 1;
+
+    if (this.heap[i] < this.heap[p]) {
+      this.swap(i, p);
+      this.bubbleUp(p);
+    }
+  }
+
+  private pop(): void {
+    if (this.heap.length === 0) return;
+
+    this.heap[0] = this.heap.pop()!;
+
+    this.bubbleDown(0);
+  }
+
+  private bubbleDown(i: number): void {
+    const left = 2 * i + 1;
+    const right = 2 * i + 2;
+
+    if (left >= this.heap.length) return;
+
+    let minIdx = left;
+
+    if (right < this.heap.length && this.heap[right] < this.heap[left]) {
+      minIdx = right;
+    }
+    if (this.heap[minIdx] < this.heap[i]) {
+      this.swap(i, minIdx);
+      this.bubbleDown(minIdx);
+    }
   }
 }
 
