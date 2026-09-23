c435977e41c298ae533c9d37e6ff1d30315e7a74
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-09-24 05:14:58 +0530

    optimized leetcode 703

--- NeetCode150/10.Heap-Priority-Queue/1.kth-largest-element-in-a-stream.ts
+++ NeetCode150/10.Heap-Priority-Queue/1.kth-largest-element-in-a-stream.ts
@@ -2,78 +2,97 @@ import { runClassTests } from "#functions/code-tester.js";
 
 // LeetCode 703
 
+class Heap<T = number> {
+  private h: T[] = [];
+
+  constructor(
+    private readonly compare: (a: T, b: T) => number = (a: any, b: any) =>
+      a - b,
+  ) {}
+
+  get size() {
+    return this.h.length;
+  }
+  peek(): T | undefined {
+    return this.h[0];
+  }
+  enqueue(val: T): void {
+    this.h.push(val);
+    this.bubbleUp(this.h.length - 1);
+  }
+  dequeue(): T | undefined {
+    if (this.h.length === 0) return undefined;
+    const top = this.h[0];
+    const bot = this.h.pop()!;
+
+    if (this.h.length) {
+      this.h[0] = bot;
+      this.bubbleDown(0);
+    }
+
+    return top;
+  }
+  replace(val: T) {
+    if (this.h.length === 0) {
+      this.enqueue(val);
+      return;
+    }
+    this.h[0] = val;
+    this.bubbleDown(0);
+  }
+
+  private swap(first: number, second: number): void {
+    [this.h[first], this.h[second]] = [this.h[second], this.h[first]];
+  }
+  private bubbleUp(i: number): void {
+    while (i > 0) {
+      const p = (i - 1) >> 1;
+      if (this.compare(this.h[i], this.h[p]) < 0) {
+        this.swap(i, p);
+        i = p;
+      } else break;
+    }
+  }
+  private bubbleDown(i: number): void {
+    const n = this.h.length;
+    while ((i << 1) + 1 < n) {
+      let b = (i << 1) + 1;
+      const r = b + 1;
+
+      if (r < n && this.compare(this.h[r], this.h[b]) < 0) b = r;
+
+      if (this.compare(this.h[b], this.h[i]) < 0) {
+        this.swap(i, b);
+        i = b;
+      } else break;
+    }
+  }
+}
+
 class KthLargest {
-  private heap: number[] = [];
-  private readonly k: number;
+  minHeap: Heap<number>;
+  k: number;
 
   constructor(k: number, nums: number[]) {
     this.k = k;
+    this.minHeap = new Heap<number>((a, b) => a - b);
 
     for (const num of nums) {
-      this.insert(num);
-
-      if (this.heap.length > k) {
-        this.pop();
-      }
+      this.add(num);
     }
   }
 
   add(val: number): number {
-    this.insert(val);
+    if (this.minHeap.size < this.k) {
+      this.minHeap.enqueue(val);
+    } else {
+      const min = this.minHeap.peek();
 
-    if (this.heap.length > this.k) {
-      this.pop();
+      if (min !== undefined && min < val) {
+        this.minHeap.replace(val);
+      }
     }
-
-    return this.heap[0];
-  }
-
-  private insert(val: number): void {
-    this.heap.push(val);
-    this.bubbleUp(this.heap.length - 1);
-  }
-
-  private swap(first: number, second: number): void {
-    [this.heap[first], this.heap[second]] = [
-      this.heap[second],
-      this.heap[first],
-    ];
-  }
-
-  private bubbleUp(i: number): void {
-    if (i === 0) return;
-
-    const p = (i - 1) >> 1;
-
-    if (this.heap[i] < this.heap[p]) {
-      this.swap(i, p);
-      this.bubbleUp(p);
-    }
-  }
-
-  private pop(): void {
-    if (this.heap.length === 0) return;
-
-    this.heap[0] = this.heap.pop()!;
-
-    this.bubbleDown(0);
-  }
-
-  private bubbleDown(i: number): void {
-    const left = 2 * i + 1;
-    const right = 2 * i + 2;
-
-    if (left >= this.heap.length) return;
-
-    let minIdx = left;
-
-    if (right < this.heap.length && this.heap[right] < this.heap[left]) {
-      minIdx = right;
-    }
-    if (this.heap[minIdx] < this.heap[i]) {
-      this.swap(i, minIdx);
-      this.bubbleDown(minIdx);
-    }
+    return this.minHeap.peek()!;
   }
 }
 
